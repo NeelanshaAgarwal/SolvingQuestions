4cefb9ffea129824f01a7dc9b1d679dfb3115608
NeelanshaAgarwal <na1027@srmist.edu.in>
2024-06-22 13:13:53 +0530

    first commit

---
+++ DSA/sorting/bubbleSort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[],int size){
+    for(int i=size-1;i>0;i--){
+        for(int j = 0; j <= i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bubbleSort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
---
+++ DSA/sorting/selectionSort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[],int size){
+    for(int i=0;i<=size-2;i++){
+        int mini = i;
+        for(int j = i; j <= size-1; j++){
+            if(arr[j]<=arr[mini]){
+                mini = j;
+            }
+        }
+        swap(arr[mini],arr[i]);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    selectionSort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
