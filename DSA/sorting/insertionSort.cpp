da218eb34cf745b226eac9692862a9535eb12614
Neelansha Agarwal <94228385+NeelanshaAgarwal@users.noreply.github.com>
2024-06-22 13:31:57 +0530

    Update insertionSort.cpp

--- DSA/sorting/insertionSort.cpp
+++ DSA/sorting/insertionSort.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 void insertionSort(int arr[],int size){
-    for(int i=size-1;i>0;i--){
-        for(int j = 0; j <= i-1; j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
-            }
+    for(int i=0;i<=size-1;i++){
+        int j = i;
+        while(j>0&&arr[j-1]>arr[j]){
+            swap(arr[j-1],arr[j]);
+            j--;
         }
     }
 }
